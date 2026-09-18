69e0af0232e44f7163a885007109545ce79e4494
Rhys B <jetpackingwolf@gmail.com>
2026-09-18 18:30:01 +1000

    Initial compatibility shim

--- src/main/java/com/legacyminecraft/poseidoncompat/PoseidonCompatPlugin.java
+++ src/main/java/com/legacyminecraft/poseidoncompat/PoseidonCompatPlugin.java
@@ -1,22 +1,18 @@
-package org.retromc.templateplugin;
+package com.legacyminecraft.poseidoncompat;
 
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.retromc.templateplugin.commands.TemplateTestCommand;
 
 import java.io.File;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class TemplatePlugin extends JavaPlugin {
+public class PoseidonCompatPlugin extends JavaPlugin {
     private JavaPlugin plugin;
     private Logger log;
     private String pluginName;
     private PluginDescriptionFile pdf;
-
-    private TemplateConfig configuration;
-
 
     @Override
     public void onEnable() {
@@ -26,16 +22,6 @@ public class TemplatePlugin extends JavaPlugin {
         pluginName = pdf.getName();
         log.info("[" + pluginName + "] Is Loading, Version: " + pdf.getVersion());
 
-        // Load configuration
-        configuration = new TemplateConfig(this, new File(getDataFolder(), "config.yml")); // Load the configuration file from the plugin's data folder
-
-        // Register the commands
-        getCommand("testcommand").setExecutor(new TemplateTestCommand(this));
-
-        // Register the listeners
-        TemplateListener listener = new TemplateListener(this);
-        getServer().getPluginManager().registerEvents(listener, this);
-
         log.info("[" + pluginName + "] Is Loaded, Version: " + pdf.getVersion());
     }
 
@@ -43,17 +29,10 @@ public class TemplatePlugin extends JavaPlugin {
     public void onDisable() {
         log.info("[" + pluginName + "] Is Unloading, Version: " + pdf.getVersion());
 
-        // Save configuration
-        //config.save(); // Save the configuration file to disk. This should only be necessary if the configuration cam be modified during runtime.
-
         log.info("[" + pluginName + "] Is Unloaded, Version: " + pdf.getVersion());
     }
 
     public void logger(Level level, String message) {
         Bukkit.getLogger().log(level, "[" + plugin.getDescription().getName() + "] " + message);
-    }
-
-    public TemplateConfig getConfig() {
-        return configuration;
     }
 }
